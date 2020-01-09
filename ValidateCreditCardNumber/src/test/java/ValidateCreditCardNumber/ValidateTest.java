0db8bbb9a761e37d14b2e95315f3c3cbceb839b2
Michał Czyż <czyzu1710@gmail.com>
2020-01-09 17:14:12 +0100

    Added some more test in TestValidate.java

--- ValidateCreditCardNumber/src/test/java/ValidateCreditCardNumber/ValidateTest.java
+++ ValidateCreditCardNumber/src/test/java/ValidateCreditCardNumber/ValidateTest.java
@@ -13,4 +13,35 @@ class ValidateTest{
     void testNumber891(){
         assertFalse(Validate.validate("891"));
     }
+
+    @Test
+    void testNumber0912562428(){
+        assertTrue(Validate.validate("0912562428"));
+    }
+
+    @Test
+    void testNumber8522170385(){
+        assertFalse(Validate.validate("8522170385"));
+    }
+
+    @Test
+    void testNumber054445(){
+        assertTrue(Validate.validate("054445"));
+    }
+
+    @Test
+    void testNumber063631(){
+        assertFalse(Validate.validate("063631"));
+    }
+
+    @Test
+    void testNumber1271(){
+        assertTrue(Validate.validate("1271"));
+    }
+
+    @Test
+    void testNumber2079(){
+        assertFalse(Validate.validate("2079"));
+    }
+
 }
